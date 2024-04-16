d8bdb8bdeeea8f498959273ec8c5d824bbb91b31
Hannes Mann <hannesmann2000@gmail.com>
2024-04-16 12:16:19 +0200

    Add a model of applications and devices

---
+++ model/app.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2024 Hannes Mann, Alexander Wigren
+# See LICENSE for details
+
+from enum import Enum
+from urllib.parse import urlparse
+
+class OperatingSystem(Enum):
+	"""Enumeration of supported mobile operating systems"""
+
+	ANDROID = 1
+	IOS = 2
+
+class Application:
+	"""Represents a mobile application"""
+
+	def __init__(self, id, os, name=None, permissions=None, store_page_url=None, privacy_policy_url=None):
+		"""
+		:param id: Application ID (Android), Bundle ID (iOS)
+		:param os: Operating system this application was developed for
+		:param name: English name of this application
+		:param permissions: Permissions granted to this application
+		:param store_page_url: Application store page
+		:param privacy_policy_url: Privacy policy provided by the developer
+		"""
+
+		self.id = id
+		self.os = os
+		self.name = name
+		self.permissions = permissions
+
+		if store_page_url:
+			self.store_page_url = urlparse(store_page_url)
+		else:
+			self.store_page_url = None
+
+		if privacy_policy_url:
+			self.privacy_policy_url = urlparse(privacy_policy_url)
+		else:
+			self.privacy_policy_url = None
+
+	# TODO: Can this be determined through MDM?
+	def is_system_app():
+		return False
+
---
+++ model/mdm.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2024 Hannes Mann, Alexander Wigren
+# See LICENSE for details
+
+from enum import Enum
+
+class DeviceOwnership(Enum):
+	"""
+	Ownership of device in MDM environment
+	This setting determines the app list available to the server
+	"""
+
+	CORPORATE_OWNED = 1
+	USER_OWNED = 2
+
+class Device:
+	"""Represents a managed user device"""
+
+	def __init__(self, id, name, os, ownership):
+		"""
+		:param id: Unique ID of this device (example: "446925a3-64b7-4d7e-9e6e-0f9ab11b31d9")
+		:param name: Name of this device (example: "Hannes_AndroidEnterprise_1/1/2024_9:00 AM")
+		:param os: Operating system used on this device
+		:param ownership: Determines if this device is corporate-owned or user-owned
+		"""
+
+		self.id = id
+		self.name = name
+		self.os = os
+		self.ownership = ownership
+		self.discovered_apps = set()
+
+	def update_discovered_apps(self, apps, replace=True):
+		"""
+		:param apps: Discovered apps from a data source such as MDM
+		:type apps: list or set
+		:param replace: If set to true, the existing set of discovered apps will be cleared
+		"""
+
+		if replace:
+			self.discovered_apps = set()
+
+		for app in apps:
+			# List of model.Application
+			if app.package_id:
+				self.discovered_apps.add(app.package_id)
+			# List of application package IDs
+			elif isinstance(app, str):
+				self.discovered_apps.add(app)
+			else:
+				raise TypeError("Discovered apps should be a list of model.Application or package IDs")
+
+	def is_app_installed(self, package_id):
+		"""
+		:param package_id: Application package ID
+		"""
+		return self.discovered_apps[package_id]
+
+	def has_user_apps(self):
+		"""
+		Determines if all apps on the device, including user apps, have been discovered
+		Only corporate-owned devices allow access to the user profile
+		"""
+		return self.ownership == DeviceOwnership.CORPORATE_OWNED and len(self.discovered_apps) > 0
