9b39ba74332b26e49b5ca4829b278d4f2b8c5581
alex <wig@mailbox.org>
2024-05-11 19:33:35 +0200

    completed GPT analysis with O'Loughlin criteria

--- analysis/gpt.py
+++ analysis/gpt.py
@@ -1,57 +1,84 @@
+import pdfkit
+
+from analysis.analyzer import AppAnalyzer
+
 from analysis.core.parsing import read_file
 from analysis.core.chunking import chunk_file
 from analysis.core.embedding import embed_files
 from analysis.core.qa import query_folder
 from analysis.core.utils import get_llm
 
-from io import BytesIO
+class GPT_Analyzer(AppAnalyzer):
+	
+	def name(self):
+		return 'GPT_Analyzer'
+	
+	def analyze_app(self, app):
 
-# Just a debug function to check reachability.
-def printathing():
-	print("hello there")
+		EMBEDDING = "openai"
+		VECTOR_STORE = "faiss"
 
-# Function to analyse the contents of a privacy policy. API key and BytesIO object must be included in function call.
-def ppcheck(openai_api_key):
+		#MODEL_LIST = ["gpt-3.5-turbo", "gpt-4"]
+		model = "gpt-4"
 
-	EMBEDDING = "openai"
-	VECTOR_STORE = "faiss"
-	MODEL_LIST = ["gpt-3.5-turbo", "gpt-4"]
+		try:
+			# Put the API key here!
+			openai_api_key = "KEY HERE"
+			
+			pdfkit.from_url(app.privacy_policy_url.scheme + "://" + app.privacy_policy_url.netloc + app.privacy_policy_url.path, 'outfile.pdf')
+			uploaded_file = open('outfile.pdf', "rb")
+			file = read_file(uploaded_file)
+			
+			# Chunk file before analysis.
+			chunked_file = chunk_file(file, chunk_size=300, chunk_overlap=0)
 
-	model = "gpt-4"
-
-	# Right now the file is hardcoded like this but this will be removed eventually and documents dynamically provided in the call.
-	uploaded_file = open('testdocu.pdf', "rb")
-
-	try:
-		file = read_file(uploaded_file)
-	except Exception as e:
-		print("FILE READ ERROR: ", e)
-
-	# This should be removed along with the open call above once properly implemented.
-	uploaded_file.close()
-
-	# Chunk file before analysis.
-	chunked_file = chunk_file(file, chunk_size=300, chunk_overlap=0)
-
-	# Index and create embeddings.
-	folder_index = embed_files(
+			# Index and create embeddings.
+			folder_index = embed_files(
 			files=[chunked_file],
 			embedding=EMBEDDING if model != "debug" else "debug",
 			vector_store=VECTOR_STORE if model != "debug" else "debug",
 			openai_api_key=openai_api_key,
 			)
 
-	# Testquery, remove once properly implemented.
-	query = "Describe in two sentences what type of document this is. Then, fill in the list on the on this format with Y for yes or N for no in the place of X for the relevant question. \nDoes the company sell user data? [X] \nDoes the company collect any type of user data? [X] \nIs the company name Facebook? [X]"
+			# This is the O'Loughlin type query.
+			query = "Fill in the list on this format with Y for yes or N for no in the place of X for the relevant question.\nDoes the company/application collect any personally identifiable information such as e-mail address, real names, phone number or location. [X]\nDoes the document mention a login process using a pincode, password or similar safety measure to use the service? [X]\nDoes the document mention that the information is stored encrypted or that it stays locally on the device? [X]\nDoes the document mention the information storage and sharing procedures OR that information is stored locally? [X]\nDoes the document mention that users can delete their information OR that information is stored locally? [X]\nDoes the document mention that users can edit their information OR that information is stored locally? [X]\nDoes the document state that users can use the service/application WITHOUT entering any personally identifiable information OR that information is stored locally? [X]"
 
-	# Send query and get response.
-	llm = get_llm(model=model, openai_api_key=openai_api_key, temperature=0)
-	result = query_folder(
-			folder_index=folder_index,
-			query=query,
-			return_all=1,
-			llm=llm,
-		)
+			# Send query and get response.
+			llm = get_llm(model=model, openai_api_key=openai_api_key, temperature=0)
+			result = query_folder(
+				folder_index=folder_index,
+				query=query,
+				return_all=1,
+				llm=llm,
+				)
+			
+			# This is a test string used to check the behavior of the O'Loughlin framework.
+			# Uncomment this and and replace the res below and the calls to llm above to test it. Refer to the paper by O'Loughling et al. for details.
+			#res = ["pid [Y]", "pid [N]", "pid [N]", "pid [N]", "pid [Y]", "pid [Y]", "pid [Y]"]
+			
+			res = str.splitlines(result.answer)
+			
+			# Check if personally identifiable information is collected.
+			if(res[0][len(res[0]) - 2] == 'Y'):
+				# Check remaining parameters.
+				if(res[1][len(res[1]) - 2] == 'N' or res[2][len(res[2]) - 2] == 'N' or res[3][len(res[3]) - 2] == 'N'):
+					#print("unacceptable!")
+					return 0
+				elif(res[4][len(res[1]) - 2] == 'N' or res[5][len(res[1]) - 2] == 'N' or res[6][len(res[1]) - 2] == 'N'):
+					#print("questionable!")
+					return 0.5
+				elif(True):
+					#print("acceptable!")
+					return 1
+			# If no personally identifiable information is collected:
+			else:
+				for line in res[1:-1]:
+					if(line[len(line) - 2] == 'N'):
+						#print("questionable!")
+						return 0.5
+			#print("acceptable!")
+			return 1
 
-	# Print the answer. This should instead be returned in final build on some predetermined format.
-	print(result.answer)
+		except Exception as e:
+			print(e)
+			return -1
