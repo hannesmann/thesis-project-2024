a6c425cb7ec6e24945351575f3ac5924304daf35
Hannes Mann <hannesmann2000@gmail.com>
2024-05-31 17:43:12 +0200

    Update configs for GPT

--- analysis/gpt.py
+++ analysis/gpt.py
@@ -37,18 +37,20 @@ class GPTAnalyzer(AppAnalyzer):
 		# with open(f"output_{app.id}.pdf", "wb") as f:
 		#	 f.write(pdf.getbuffer())
 
-		# Chunk file before analysis.
-		chunked_file = chunk_file(file, chunk_size=300, chunk_overlap=0)
-
 		model = configs.main.analysis.gpt.model
+		embedding_model = configs.main.analysis.gpt.embedding_model
 		api_key = configs.secrets.api.openai
 
-		# Index and create embeddings.
+		# Chunk file before analysis
+		chunked_file = chunk_file(file, chunk_size=300, chunk_overlap=0, model_name=model)
+
+		# Index and create embeddings
 		folder_index = embed_files(
 			files=[chunked_file],
 			embedding=EMBEDDING if model != "debug" else "debug",
 			vector_store=VECTOR_STORE if model != "debug" else "debug",
 			openai_api_key=api_key,
+			model=embedding_model
 		)
 
 		# This is the O'Loughlin type query.
@@ -61,18 +63,17 @@ class GPTAnalyzer(AppAnalyzer):
 			Does the document mention that users can edit their information OR that information is stored locally? [X]
 			Does the document state that users can use the service/application WITHOUT entering any personally identifiable information OR that information is stored locally? [X]"""
 
-		# Send query and get response.
 		llm = get_llm(model=model, openai_api_key=api_key, temperature=0)
 		result = query_folder(
 			folder_index=folder_index,
 			query=query,
 			return_all=1,
 			llm=llm,
-			)
+		)
 
 		# This is a test string used to check the behavior of the O'Loughlin framework.
 		# Uncomment this and and replace the res below and the calls to llm above to test it. Refer to the paper by O'Loughling et al. for details.
-		#res = ["pid [Y]", "pid [N]", "pid [N]", "pid [N]", "pid [Y]", "pid [Y]", "pid [Y]"]
+		# res = ["pid [Y]", "pid [N]", "pid [N]", "pid [N]", "pid [Y]", "pid [Y]", "pid [Y]"]
 
 		res = str.splitlines(result.answer)
 
@@ -80,22 +81,18 @@ class GPTAnalyzer(AppAnalyzer):
 			logger.info(f"Response from GPT: {result.answer}")
 
 		# Check if personally identifiable information is collected.
-		if(res[0][len(res[0]) - 2] == 'Y'):
+		if res[0][len(res[0]) - 2] == 'Y':
 			# Check remaining parameters.
-			if(res[1][len(res[1]) - 2] == 'N' or res[2][len(res[2]) - 2] == 'N' or res[3][len(res[3]) - 2] == 'N'):
-				#print("unacceptable!")
+			if res[1][len(res[1]) - 2] == 'N' or res[2][len(res[2]) - 2] == 'N' or res[3][len(res[3]) - 2] == 'N':
 				return 1
-			elif(res[4][len(res[4]) - 2] == 'N' or res[5][len(res[5]) - 2] == 'N' or res[6][len(res[6]) - 2] == 'N'):
-				#print("questionable!")
+			elif res[4][len(res[4]) - 2] == 'N' or res[5][len(res[5]) - 2] == 'N' or res[6][len(res[6]) - 2] == 'N':
 				return 0.5
-			elif(True):
-				#print("acceptable!")
+			elif True:
 				return 0
 		# If no personally identifiable information is collected:
 		else:
 			for line in res[1:-1]:
 				if(line[len(line) - 2] == 'N'):
-					#print("questionable!")
 					return 0.5
-		#print("acceptable!")
+
 		return 0
